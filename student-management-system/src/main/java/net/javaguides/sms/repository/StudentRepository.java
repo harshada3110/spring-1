74d8da92f94facfe4c9882a2fe7c3d8ce9ca6e8d
harshada3110 <47829004+harshada3110@users.noreply.github.com>
2023-06-03 22:50:22 +0530

    Add files via upload

---
+++ student-management-system/src/main/java/net/javaguides/sms/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package net.javaguides.sms.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.javaguides.sms.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+	Optional<Student> findByEmail(String email);
+
+}
